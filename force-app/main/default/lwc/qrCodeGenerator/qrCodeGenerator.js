29325ceb843dc3cd3893aaa9e655785a84358faa
Andres Perez <andresperezf@yahoo.com>
2019-12-14 09:38:47 -0500

    Added QR

--- force-app/main/default/lwc/qrCodeGenerator/qrCodeGenerator.js
+++ force-app/main/default/lwc/qrCodeGenerator/qrCodeGenerator.js
@@ -1,10 +1,10 @@
 import { LightningElement, api } from "lwc";
-import QRCode from "@salesforce/resourceUrl/qrCode"; // https://github.com/soldair/node-qrcode
+import QRCodeLib from "@salesforce/resourceUrl/QRCodeLib"; // https://github.com/soldair/node-qrcode
 import { loadScript } from "lightning/platformResourceLoader";
 
 export default class qrCodeGenerator extends LightningElement {
 	_recordId;
-	_initialized = false;
+	_ready = {};
 
 	@api
 	get recordId() {
@@ -12,32 +12,38 @@ export default class qrCodeGenerator extends LightningElement {
 	}
 	set recordId(value) {
 		this._recordId = value;
+		this._ready.recordId = true;
 		this.generateQR();
 	}
 
-	renderedCallback() {
-		if (!this._initialized) {
-			this._initialized = true;
-			loadScript(this, QRCode);
-		}
+	constructor() {
+		super();
+		loadScript(this, QRCodeLib + "/qrcode.min.js").then(() => {
+			this._ready.staticResource = true;
+			this.generateQR();
+		});
 	}
 
 	generateQR() {
-		const canvas = this.template.querySelector('[data-id="QRCode"]');
-		const sData = JSON.stringify({
-			copy1: this.recordId,
-			copy2: this.recordId,
-			dttm: new Date().toJSON()
-		});
-
-		QRCode.toCanvas(canvas, sData, { margin: 0, width: Math.min(window.innerWidth, window.innerHeight) * 0.9 })
-			// QRCode.toCanvas(canvas, sData)
-			.then(() => {
-				// eslint-disable-next-line no-console
-				console.log("Generated QR!");
-			})
-			.catch(err => {
-				throw Error(err);
+		if (this._ready.staticResource && this._ready.recordId) {
+			const card = this.template.querySelector('[data-id="card"]');
+			const canvas = this.template.querySelector('[data-id="QRCode"]');
+			const sData = JSON.stringify({
+				copy1: this.recordId,
+				copy2: this.recordId,
+				dttm: new Date().toJSON()
 			});
+
+			// eslint-disable-next-line no-undef
+			QRCode.toCanvas(canvas, sData, { margin: 0, width: card.width * 0.9 })
+				// QRCode.toCanvas(canvas, sData)
+				.then(() => {
+					// eslint-disable-next-line no-console
+					console.log("Generated QR!");
+				})
+				.catch(err => {
+					throw Error(err);
+				});
+		}
 	}
 }
